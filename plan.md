60e3c76d6a08e8859b80aff41210fc2e0d5ecd85
bahirithikaramp <bahirithi.karampudi@gmail.com>
2021-06-19 16:54:06 +0530

    Added a unique room id

--- plan.md
+++ plan.md
@@ -2,7 +2,7 @@
 
 - Initialise our NodeJS project  (DONE)
 - Initialise our first view (what we will see on frontend)   (DONE)
-- Create a room id
+- Create a room id    (DONE)
 - Add the  ability to view our own video
 - Add ability for others to stream their video
 - Add styling 
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const { request } = require('http');
 const app = express();
 const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('room');
+    res.redirect(`/${uuidv4()}`);
 })
 
-
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
 
 
 server.listen(3030); // localhost 3030
