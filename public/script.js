e02abbbe463f5fb0a4d9ff935eb6f2ca4a6e0c17
bahirithikaramp <bahirithi.karampudi@gmail.com>
2021-06-19 22:19:32 +0530

    add ability for others to stream their videos

--- public/script.js
+++ public/script.js
@@ -1,8 +1,16 @@
 // JS for the frontend
 
+const socket = io('/');
+
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
+
+var peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '3030'
+}); 
 
 // promise to access the audio and video
 let myVideoStream
@@ -12,7 +20,29 @@ navigator.mediaDevices.getUserMedia({
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+    peer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+    socket.on('user-connected', (userId) => {
+        connectToNewUser(userId, stream);
+    })
 })
+
+peer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
+})
+
+const connectToNewUser = (userId, stream) => {
+    const call = peer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+}
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
