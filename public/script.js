1a57a7011b2ac363c2bb554756837ec1005031ca
bahirithikaramp <bahirithi.karampudi@gmail.com>
2021-06-20 16:46:59 +0530

    Adding play and stop button

--- public/script.js
+++ public/script.js
@@ -101,3 +101,29 @@ const setUnmuteButton = () => {
     document.querySelector('.main__mute_button').innerHTML = html;
 }
 
+const playStop = () => {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo()
+    } else {
+        setStopVideo()
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+const setStopVideo = () => {
+    const html = `
+        <i class="play fas fa-video"></i>
+        <span>Stop Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+    const html = `
+        <i class="stop fas fa-video-slash"></i>
+        <span>Play Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
