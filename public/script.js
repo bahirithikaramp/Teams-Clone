aeb39e295ca04e06bcd5a3d4f276e6ad92880902
bahirithikaramp <bahirithi.karampudi@gmail.com>
2021-06-20 12:32:12 +0530

    Adding Mute and Unmute button

--- public/script.js
+++ public/script.js
@@ -74,4 +74,30 @@ const scrollToBottom = () => {
     d.scrollTop(d.prop("scollHeight"));
 }
 
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    } else {
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+const setMuteButton = () => {
+    const html = `
+        <i class="fas fa-microphone"></i>
+        <span>Mute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+    const html = `
+        <i class="unmute fas fa-microphone-slash"></i>
+        <span>Unmute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
 
